61687f7e3c78b704a7c5c2c0126ecf903c88e9f8
Georgi Berchev <g.berchev@gmail.com>
2018-12-06 15:39:31 +0200

    code added

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "null" {}
+
+resource "null_resource" "georgiman"{
+  triggers {
+  build_number = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    command = "echo hello georgiman"
+  }
+}
